87bfaa0502c05a6ceedaa05f8c3818515f58c579
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-09-19 18:56:34 +0300

    feat: add Bean Validation annotations to Task record

--- src/main/java/linx7a/task_management_system/model/Task.java
+++ src/main/java/linx7a/task_management_system/model/Task.java
@@ -1,14 +1,22 @@
 package linx7a.task_management_system.model;
 
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record Task(
         Long id,
+        @NotNull
         Long creatorId,
         Long assignedUserId,
         Status status,
         LocalDateTime createDateTime,
+        @NotNull
+        @Future
         LocalDateTime deadlineDate,
+        @NotNull
         Priority priority
 ) {
 }
